69545cd34cd142fbb977b39e419f8a429fe2b33a
Nguyễn Minh Nhật <minhnhatmn16@gmail.com>
2025-05-31 20:15:28 +0700

    BE: upload pdf

--- BKDNOJ_FE_BE/BKDNOJ_BE/src/middleware/multer.js
+++ BKDNOJ_FE_BE/BKDNOJ_BE/src/middleware/multer.js
@@ -8,6 +8,14 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF files are allowed!"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
 
 module.exports = upload;
